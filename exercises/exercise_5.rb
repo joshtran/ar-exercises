21e435e07c5dc988ae85ae92212c408ef9535c9c
Joshua Tran <joshuatrain.com>
2017-02-15 04:55:29 +0000

    Building queries based on sales data.

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.sum(:annual_revenue)
+puts "Total revenue: #{@total_revenue}"
+
+@average_revenue = Store.average(:annual_revenue)
+puts "Average revenue: #{@average_revenue}"
+
+@high_sales = Store.where("annual_revenue > ?", 1000000).count
+puts "Number of million dollar stores: #{@high_sales}"
